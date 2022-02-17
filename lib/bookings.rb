b41c4734bda1f5a802ee83e257a19dab77d52980
Barrington-Brook <bbmxkrs@gmail.com>
2022-02-17 14:18:42 +0000

    Refactor Booking class

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -3,19 +3,21 @@ require 'pg'
 class Bookings
   attr_reader :start_date, :end_date
 
+  @@connection = nil
+
+  def self.connect(dbname)
+    @@connection = PG.connect(dbname: db)
+  end
+
   def self.list_bookings
-    connection = PG.connect(dbname: "mcairbnb")
-    result = connection.exec('SELECT start_date, end_date FROM bookings;')
+    result = @@connection.exec('SELECT start_date, end_date FROM bookings;')
     result.map{ |booking| booking}
   end
 
     attr_reader :booked
 
-  def self.booked(start_date, end_date, db = 'mcairbnb')
-    @start_date = start_date
-    @end_date = end_date
-    connection = PG.connect(dbname: db)
-    connection.exec("INSERT INTO bookings (start_date, end_date) VALUES ('#{@start_date}', '#{@end_date}');")
+  def self.booked(start_date, end_date)
+    @@connection.exec("INSERT INTO bookings (start_date, end_date) VALUES ('#{start_date}', '#{end_date}');")
     connection
   end
 
